a1b5c7bd7821936c81335671a87879beb05d3cc7
kristinna444 <krisi.gramatikova15@gmail.com>
2025-08-20 22:39:29 +0300

    Upload exam variant 2

---
+++ Exams/Exam Variant 2/Task.01 & 03.cpp
@@ -0,0 +1,181 @@
+﻿// Exam Variant 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+constexpr int MAX_SIZE = 10;
+constexpr int LEN = 3;
+
+//Task.1
+long getAbsValue(long num) {
+    return num < 0 ? -num : num;
+}
+
+long powerOf(int num, int pow) {
+    long multiplier = 1;
+
+    while (pow) {
+        multiplier *= num;
+        pow--;
+    }
+
+    return multiplier;
+}
+
+int findNumOfDigits(long num) {
+    if (num == 0) return 1;
+    
+    int count = 0;
+
+    while (num) {
+        count++;
+        num /= 10;
+    }
+
+    return count; 
+}
+
+long concat(long a, long b, int len) {
+    int multiplier = powerOf(10, len);
+    return a * multiplier + b; 
+}
+
+long findNewNum(long num, int index) {
+    if (index < 0 || index >= findNumOfDigits(num)) {
+        return num;
+    }
+
+    long divisor = powerOf(10, index); 
+    long remain = num % divisor;  
+    
+    divisor *= 10;
+    num /= divisor; 
+
+    return concat(num, remain, index); 
+}
+
+bool isMonotone(long num) {
+    int right = num % 10;
+    num /= 10;
+
+    bool increases = true, decreases = true;
+
+    while (num) {
+        int left = num % 10;
+
+        if (right < left) {
+            increases = false;
+        }
+
+        if (right > left) {
+            decreases = false;
+        }
+
+        right = left; 
+        num /= 10;
+    }
+
+    return increases || decreases; 
+}
+
+bool checkNum(long num) {
+    num = getAbsValue(num); 
+
+    int digits = findNumOfDigits(num);
+    if (digits <= 2) return true;
+
+    for (int i = 0; i < digits; i++) {
+        long newNum = findNewNum(num, i);
+
+        if (isMonotone(newNum)) { 
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//Task.3
+int* spiralRead(const int** matrix, size_t rows, size_t cols) {
+    if (!matrix || rows == 0 || cols == 0) {
+        return nullptr;
+    }
+
+    int top = 0;
+    int bottom = rows - 1;
+    int left = 0;
+    int right = cols - 1;
+
+    size_t len = rows * cols;
+    int* arr = new int[len]; 
+    int index = 0;  
+
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right && index < len; i++) {
+            arr[index++] = matrix[bottom][i];
+        }
+
+        bottom--;
+
+        for (int i = bottom; i >= top && index < len; i--) { 
+            arr[index++] = matrix[i][right];
+        }
+
+        right--;
+
+        if (top <= bottom) {
+            for (int i = right; i >= left && index < len; i--) {
+                arr[index++] = matrix[top][i];
+            }
+
+            top++;
+        }
+
+        if (left <= right) {
+            for (int i = top; i <= bottom && index < len; i++) {
+                arr[index++] = matrix[i][left];
+            }
+
+            left++;
+        }
+    }
+     
+    return arr;
+}
+
+//Helper functions
+int** readMatrix(size_t rows, size_t cols) {
+    if (rows == 0 || cols == 0) {
+        return nullptr;
+    }
+
+    int** matrix = new int* [rows];
+
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new int[cols];
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    return matrix;
+}
+
+void freeMatrix(int** matrix, size_t rows) {
+    for (int i = 0; i < rows; i++) {
+        delete[] matrix[i];
+    }
+
+    delete[] matrix;
+}
+
+void printArr(const int* arr, size_t len) {
+    if (!arr) return;
+
+    for (int i = 0; i < len; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
---
+++ Exams/Exam Variant 2/Task.02.cpp
@@ -0,0 +1,148 @@
+﻿// Task 02 variant 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+bool isDigit(char ch) {
+    return '0' <= ch && ch <= '9';
+}
+
+int charToDigit(char ch) {
+    return ch - '0';
+}
+
+int charToNumber(const char* ptr) {
+    if (!ptr) return 0;
+
+    int result = 0;
+
+    while (*ptr) {
+        if (!isDigit(*ptr)) {
+            cout << "The string must consist only of digits!" << endl;
+            return 0;
+        }
+
+        result *= 10;
+        result += charToDigit(*ptr);
+        ptr++;
+    }
+
+    return result;
+}
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void sortArray(int* arr, int len) {
+    if (!arr) return;
+
+    for (int i = 0; i < len; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < len; j++) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) {
+            swap(arr[minIndex], arr[i]);
+        }
+    }
+}
+
+int arrToNum(const int* arr, int len) {
+    if (!arr) return -1;
+
+    int result = 0;
+    for (int i = 0; i < len; i++) {
+        result *= 10;
+        result += arr[i];
+    }
+
+    return result;
+}
+
+int handleUnique2(const int count[10]) {
+    int pair = -1, single = -1;
+    for (int i = 0; i < 10; i++) {
+        if (count[i] == 2) {
+            pair = i;
+        }
+        else if (count[i] == 1) {
+            single = i;
+        }
+    }
+
+    if (pair == 0) {
+        return single * 1000 + single;
+    }
+
+    return pair * 101 + single * 10;
+}
+
+int handleUnique3(const int count[10]) {
+    int num[3] = { 0 };
+    int index = 0;
+
+    for (int i = 0; i < 10 && index < 3; i++) {
+        if (count[i]) {
+            num[index++] = i;
+        }
+    }
+
+    sortArray(num, 3);
+
+    if (num[0] == 0) {
+        swap(num[0], num[1]);
+    }
+
+    int result = arrToNum(num, 3);
+    result = result * 10 + num[1];
+    result = result * 10 + num[0];
+
+    return result;
+}
+
+int findNumber(const char* str) {
+    if (!str) return -1;
+
+    if (!str[0] || !str[1] || !str[2] || str[3]) return -1;
+
+    int count[10] = { 0 };
+
+    for (const char* ptr = str; *ptr; ptr++) {
+        if (!isDigit(*ptr)) {
+            return -1;
+        }
+
+        int digit = charToDigit(*ptr);
+        count[digit]++;
+    }
+
+    int unique = 0;
+    for (int i = 0; i < 10; i++) {
+        if (count[i] > 0) {
+            unique++;
+        }
+    }
+
+    if (unique == 1) {
+        return charToNumber(str);
+    }
+
+    if (unique == 2) {
+        return handleUnique2(count);
+    }
+
+    return handleUnique3(count);
+}
+
+int main()
+{
+    cout << findNumber("103") << endl;
+}
+
